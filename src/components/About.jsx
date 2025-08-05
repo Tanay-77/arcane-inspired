944d86138472fdcb256c6cc52b9c1666b042d753
Tanay-77 <tanaymahajan7@gmail.com>
2025-08-05 23:59:38 +0530

    about section edited

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div id='about' className='min-h-screen w-screen'>
+        <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
+            <h2 className='font-general text-sm uppercase md:tex-[10px]'>WE ARE VALORANT </h2>
+            <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem] '>
+                 A 5v5 t<b>a</b>ctical shooter featuring agents <b>w</b>ith unique abilities
+            </div>
+      
+        </div>
+    </div>
+  )
+}
+
+export default About
