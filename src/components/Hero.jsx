a6bb1262e2bf2ed1d7ee089d770b95d7f9495de7
Tanay-77 <tanaymahajan7@gmail.com>
2025-08-05 23:28:49 +0530

    final hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,9 @@ import { useState, useEffect } from 'react'
 import Button from './Button'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
 
 export const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1)
@@ -22,6 +25,12 @@ export const Hero = () => {
     setCurrentIndex(upcomingVideoIndex)
     
    }
+
+   useEffect(()=>{
+    if(loadedVideos === totalVideos-1){
+        setIsLoading(false)
+    }
+   },[loadedVideos])
    
    useGSAP(()=>{
     if(hasClicked){
@@ -46,6 +55,26 @@ export const Hero = () => {
     }
 
    },{dependencies :[currentIndex] , revertOnUpdate:true})
+
+   useGSAP(()=>{
+    gsap.set('#video-frame' , {
+      clipPath : 'polygon(14% 0%, 72% 0%,90% 100%,0% 100%)',
+      borderRadius :'0 0 40% 10% ',
+    })
+
+    gsap.from('#video-frame',{
+      clipPath : 'polygon(0% 0%, 100% 0%,100% 100%,0% 100%)',
+      borderRadius :'0 0 0% 0% ',
+      ease : 'power1.inOut',
+      scrollTrigger : {
+        trigger : '#video-frame',
+        start : 'center center',
+        end : 'bottom center',
+        scrub : true,
+      }
+    })
+   })
+
    const getVideoSrc = (index) => `videos/hero-${index}.mp4`
 
    const handleVideoLoad = () => {
@@ -54,6 +83,17 @@ export const Hero = () => {
 
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
+
+      {isLoading && (
+        <div className='flex-center absolute z-[100] h-dvh w-screen
+        overflow-hidden bg-violet-50 '>
+          <div className='three-body'>
+            <div className='three-body__dot' />
+            <div className='three-body__dot'/>
+             <div className='three-body__dot'/>
+          </div>
+        </div>
+      )}
      <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden 
      rounded-lg bg-blue-75 '>
         <div className='mask-clip-path absolute-center absolute z-[60] size-64
@@ -93,18 +133,20 @@ export const Hero = () => {
           onLoadedData={handleVideoLoad}
           id='current-video'
         />
-     </div>
-     <h1 className='font-valorant hero-heading absolute bottom-5 tracking-tighter
-     right-5 z-40 text-blue-100  px-5 ' style={{fontFamily: '"valorant", "Arial Black", sans-serif'}}>SEASON 2025</h1>
-     <div className='absolute left-0 top-0 z-40 size-full pointer-events-none'>
+         <h1 className='font-valorant hero-heading absolute bottom-5 tracking-tighter
+        right-5 z-40 text-blue-100 px-5 ' style={{fontFamily: '"valorant", "Arial Black", sans-serif'}}>SEASON 2025</h1>
+        <div className='absolute left-0 top-0 z-40 size-full pointer-events-none'>
         <div className='mt-24 px-5 sm:px-10 pointer-events-auto'> 
           <h1 className='font-valorant hero-heading text-blue-100 tracking-tighter' style={{fontFamily: '"valorant", "Arial Black", sans-serif'}} >
               valora<b>n</b>t 
-              
           </h1>
           <Button id='play btn' title='Play Now' containerClass='!bg-pink-100 flex-center gap-1 '  />
         </div>
       </div>
+     </div>   
+     
+        <h1 className='font-valorant hero-heading absolute bottom-5 tracking-tighter
+        right-5  text-black px-5 ' style={{fontFamily: '"valorant", "Arial Black", sans-serif'}}>SEASON 2025</h1>
     </div >
   )
 }
