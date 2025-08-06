29eb6840b61f7149c18f130306cf32ef28e9520f
Tanay-77 <tanaymahajan7@gmail.com>
2025-08-06 23:30:47 +0530

    About Section Completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,54 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+import AnimatedTitle from './AnimatedTitle'
 import React from 'react'
 
+
 const About = () => {
+  useGSAP(()=>{
+    const clipAnimation = gsap.timeline({
+      scrollTrigger:{
+        trigger:'#clip',
+        start:'center center',
+        end:'+=800 center',
+        scrub:0.5,
+        pin : true,
+        pinSpacing :true,
+      }
+    })
+
+    clipAnimation.to('.mask-clip-path',{
+      width:'100vw',
+      height:'100vh',
+      borderRadius : 0
+    })
+  })
+
+
   return (
     <div id='about' className='min-h-screen w-screen'>
         <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
             <h2 className='font-general text-sm uppercase md:tex-[10px]'>WE ARE VALORANT </h2>
-            <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem] '>
-                 A 5v5 t<b>a</b>ctical shooter featuring agents <b>w</b>ith unique abilities
+            <AnimatedTitle  title=" A 5v5 t<b>a</b>ctical shooter featuring agents <b>w</b>ith unique abilities" 
+            containerClass="mt-5 !text-black text-center" />
+            
+
+
+            <div className='about-subtext'>
+              <p>The game of games</p>
+              <p>VALORANT</p>
             </div>
+            
       
+        </div>
+        <div className='h-dvh w-screen' id='clip' >
+          <div className='mask-clip-path about-image '>
+            <img src="img/about.webp" alt="background" className='absolute left-0 top-0
+            size-full object-cover ' />
+          </div>
         </div>
     </div>
   )
