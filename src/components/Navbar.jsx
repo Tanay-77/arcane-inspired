386e7fedb9a5b32b0915365322c7a60d106ce57d
Tanay-77 <tanaymahajan7@gmail.com>
2025-08-12 23:22:45 +0530

    navbar started

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import {useEffect,useRef} from 'react'
+import Button from './Button'
+
+const Navbar = () => {
+    const navContainerRef = useRef(null)
+
+  return (
+    <div ref={navContainerRef} className='fixed inset-x-0 top-4
+    z-50 h-16 border-none transition-all duration-700 sm:inset-x-6 '>
+        <header className='absolute top-1/2 w-full -translate-y-1/2 '>
+            <nav className='flex w-full h-full items-center justify-between
+            p-4'>
+                <div className='flex items-center gap-7'>
+
+                     <img src="/img/riot.png" alt="logo" 
+                    className='w-10 '/>
+
+                    <img src="/img/logo.png" alt="logo" 
+                    className='w-10 '/>
+
+                
+ 
+                </div>
+            </nav>
+
+        </header>
+    </div>
+  )
+}
+
+export default Navbar
