2189875644df657d31c6c671a559e2bbe08b467c
Tanay-77 <tanaymahajan7@gmail.com>
2025-08-17 19:55:47 +0530

    navbar completed

---
+++ src/components/Features.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Features = () => {
+  return (
+    <section className='bg-black pb-52'>
+        <div className='container mx-auto px-3 md:px-10'>
+            <div className='px-5 py-32'>
+                <p className='font-circular-web text-lg text-blue-100'>
+                    YOUR AGENTS
+                </p>
+            </div>
+            <p className='max-w-md font-circular-web text-lg
+            text-blue-100 opacity-50'>
+                More than guns and bullets, you’ll choose an Agent armed with adaptive, swift, and lethal abilities that create opportunities to let your gunplay shine. No two Agents play alike, 
+                just as no two highlight reels will look the same.
+            </p>
+
+        </div>
+
+    </section>
+  )
+}
+
+export default Features
