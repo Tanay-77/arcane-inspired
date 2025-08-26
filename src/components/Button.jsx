c75280e05de676d5d551ca83b2dbaa489b0df517
Tanay-77 <tanaymahajan7@gmail.com>
2025-08-26 23:49:37 +0530

    add maps

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Button({id,title,containerClass}){
   return (
-    <button id={id} className={`group relative z-10 w-fit  cursor-pointer
-        overflow-hidden px-10 py-5 text-white ml-5 mt-5 ${containerClass} `}>
+    <button id={id} className={`group relative z-10 w-fit cursor-pointer
+        overflow-hidden px-10 py-5 text-white ${containerClass} `}>
             <span className='relative inline-flex overflow-hidden font-valorant
             text-lg uppercase '>
                 <div>
--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import AnimatedTitle from './AnimatedTitle'
+import Button from './Button'
 const Story = () => {
   return (
     <section id="story" className='min-h-dvh w-screen bg-black
@@ -17,10 +18,22 @@ const Story = () => {
           <div className='story-img-container'>
             <div className='story-img-mask'>
               <div className='story-img-content'>
-                <img src="/img/entrance.webp" alt="img" />
+                <img src="/img/entrance.webp" alt="img" className='object-contain' />
               </div>
             </div>
 
+          </div>
+
+        </div>
+        <div className='-mt-80 flex w-full justify-center md:-mt-64
+        md:me-44 md:justify-end'>
+          <div className='flex h-full w-fit flex-col items-center md:items-start'>
+            <p className='mt-3 max-w-sm text-center font-circular-web
+            text-violet-100 md:text-start'>Each map is a playground to showcase your creative thinking. Purpose-built for team strategies, spectacular plays, and clutch moments. Make the play others will imitate for years to come..</p>
+           
+           <div className="w-full flex justify-center md:justify-start mt-4">
+             <Button id="realm-button" title="VIEW ALL MAPS" containerClass="bg-pink-100"/>
+           </div>
           </div>
 
         </div>
