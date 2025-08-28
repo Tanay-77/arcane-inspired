a431243ca54d80f5012dd05d4c174c0bd9e67152
Tanay-77 <tanaymahajan7@gmail.com>
2025-08-28 23:23:55 +0530

    complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -83,7 +83,7 @@ const Navbar = () => {
 
                   <button className='ml-10 flex items-center space-x-0.5' 
                   onClick={toggleAudioIndicator}>
-                    <audio ref={audioElementRef} className='hidden' src="/audio/loop.mp3" loop />
+                    <audio ref={audioElementRef} className='hidden' src="/audio/loop.mp3"  />
                     {[1,2,3,4].map((bar)=>(
                       <div key={bar} className={`indicator-line ${isIndicatorActive ? 'active' : ''}`} style={{
                         animationDelay : `${bar} * {0.1}s`}}
